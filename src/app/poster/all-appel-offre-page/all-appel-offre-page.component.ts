c0dffb6d88e51457ef4d410da42532cdc0009fd6
gracedivinealontsi <divinealontsi@gmail.com>
2020-06-18 12:13:00 +0100

    creation de la page des offres sur la branche createtest 2

--- src/app/poster/all-appel-offre-page/all-appel-offre-page.component.ts
+++ src/app/poster/all-appel-offre-page/all-appel-offre-page.component.ts
@@ -9,6 +9,28 @@ export class AllAppelOffrePageComponent implements OnInit {
 
   constructor() { }
 ngOnInit(): void {
+  $(document).ready(function(){
+
+    var list = $(".list li");
+    var numToShow = 3;
+    var button = $("#next");
+    var numInList = list.length;
+    list.hide();
+    if (numInList > numToShow) {
+      button.show();
+    }
+    list.slice(0, numToShow).show();
+
+    button.click(function(){
+        var showing = list.filter(':visible').length;
+        list.slice(showing - 1, showing + numToShow).fadeIn();
+        var nowShowing = list.filter(':visible').length;
+        if (nowShowing >= numInList) {
+          button.hide();
+        }
+    });
+
+});
  
  // SEARCH FUNCTION
 var btsearch = {
